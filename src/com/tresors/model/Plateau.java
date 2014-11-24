94175d47a86ee18d07984395515d3bedad3b6590
Arthur Veys <veysarthur@gmail.com>
2014-11-24 14:47:29 +0100

    Refractor sur la classe jeu : nommée Plateau

--- src/com/tresors/model/Plateau.java
+++ src/com/tresors/model/Plateau.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Created by arthurveys on 21/11/14.
  * Projet java ${PROJECT}
  */
-public class Jeu {
+public class Plateau {
     private ArrayList<Navire> listJoueurs;
     private Case[][] carte;
 }
