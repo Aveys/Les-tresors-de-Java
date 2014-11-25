8fce4003d0f0367d7a8dd029aeba7c2bdf1ddf97
Paul <pribierre@gmail.com>
2014-11-25 14:29:13 +0100

    Initialisation de la grille basique

--- src/com/tresors/controller/ControllerJeu.java
+++ src/com/tresors/controller/ControllerJeu.java
@@ -5,4 +5,22 @@ package com.tresors.controller;
  * Projet java ${PROJECT}
  */
 public class ControllerJeu{
+    private Case grille[][];
+
+    public void initGrille(){
+        for (int i = 0; i< 9 ; i++){
+            for (int j = 0 ; j < 9 ; j++){
+                this.grille[i][j] = new Case(i,j);
+            }
+        }
+    }
+
+    public void afficheGrille(){
+        for (int i = 0; i< 9 ; i++){
+            for (int j = 0 ; j < 9 ; j++){
+                System.out.println(grille[i][j].toString());
+            }
+        }
+    }
+
 }
