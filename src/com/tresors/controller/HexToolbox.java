c09d0200337a8deb5bfe19c5e65a4d8079cb35a8
Arthur Veys <veysarthur@gmail.com>
2014-11-25 15:01:00 +0100

    Ebauche de la toolbox pour les hexagones

--- src/com/tresors/controller/HexToolbox.java
+++ src/com/tresors/controller/HexToolbox.java
@@ -1,8 +1,47 @@
 package com.tresors.controller;
 
+import com.tresors.model.Case;
+import com.tresors.model.Mer;
+import com.tresors.model.Repaire;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 /**
  * Created by arthurveys on 25/11/14.
  */
 public class HexToolbox {
+
+
+    public static ArrayList<Repaire> templateRepaire;
+    /**
+     * Donne les coordonnées des cases voisines
+     * @param source Les coordonnées de départ
+     * @return Un tableau de point avec les coordonnées des voisins
+     */
+    public static ArrayList<Point> detecteVoisins(Point source){
+        ArrayList<Point> res = new ArrayList<Point>();
+         int neighbors[][]={{+1,  0},{+1, -1},{ 0, -1},{-1,  0},{-1, +1},{ 0, +1}};
+        for (int[] calc : neighbors){
+            res.add(new Point(source.x+calc[0],source.y+calc[1]));
+        }
+        return res;
+    }
+
+    /**
+     *
+     * @param hex1
+     * @param hex2
+     * @return
+     */
+    public static int distanceHex(Point hex1, Point hex2){
+        return ((Math.abs(hex1.y-hex2.x)+ Math.abs(hex1.y-hex2.y))+Math.abs(hex1.x+hex1.y-hex2.x-hex2.y))/2;
+    }
+
+    public static boolean estNaviguable(Case[][] plateau, Point p) {
+        Case c = plateau[p.x][p.y];
+        return !(c instanceof Repaire) && c != null;
+
+    }
 
 }
