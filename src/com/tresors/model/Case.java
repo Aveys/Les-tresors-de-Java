a3ca645a3a775c6e84449b4b94ad7b2c1cb075c8
Paul <pribierre@gmail.com>
2014-11-25 14:15:13 +0100

    First

--- src/com/tresors/model/Case.java
+++ src/com/tresors/model/Case.java
@@ -1,9 +1,19 @@
 package com.tresors.model;
 
+import java.awt.*;
+
 /**
  * Created by arthurveys on 21/11/14.
  * Projet java ${PROJECT}
  */
 public abstract class Case {
-    int z;
+
+    private Point coord= new Point();
+
+    public Case(Point point) {
+        this.coord = point;
+    }
+    public Case(int x, int y) {
+        this.coord = new Point(x,y);
+    }
 }
