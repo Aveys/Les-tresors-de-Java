eb803fa0b16e070a33426d9a405e4cc7ced1c363
Paul <pribierre@gmail.com>
2014-12-07 00:49:30 +0100

    Commit

--- src/com/tresors/repair/model/RepairChangeNbPirateEvent.java
+++ src/com/tresors/repair/model/RepairChangeNbPirateEvent.java
@@ -3,7 +3,10 @@ package com.tresors.repair.model;
 import javax.swing.event.ChangeEvent;
 
 /**
+ * A class that extends ChangeEvent and allows an event to modify the model
  * Created by Paul on 30/11/2014.
+ * @author Paul Ribierre
+ * @see javax.swing.event.ChangeEvent
  */
 
 public class RepairChangeNbPirateEvent extends ChangeEvent {
