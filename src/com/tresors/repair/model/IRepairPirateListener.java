b240d196f4ecf4067cd65dbdc18c4995f9e0dc3e
Paul <pribierre@gmail.com>
2014-12-07 00:47:31 +0100

    Commit Javadoc Ajout de Javadoc

--- src/com/tresors/repair/model/IRepairPirateListener.java
+++ src/com/tresors/repair/model/IRepairPirateListener.java
@@ -1,6 +1,11 @@
 package com.tresors.repair.model;
 import java.util.EventListener;
 
+/**
+ * An Interface that contains the methods that should be implemented when an Event asks for a modification of the model
+ * Created by Paul on 06/12/2014.
+ * @author Paul Ribierre
+ */
 public interface IRepairPirateListener extends EventListener {
     public void piratesChanged(RepairChangeNbPirateEvent event);
     public void piratesDecreased(RepairChangeNbPirateEvent event);
