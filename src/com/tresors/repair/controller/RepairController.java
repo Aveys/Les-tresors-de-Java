f383747888409ef103b230633ccf8d75cf8b95d5
Paul <pribierre@gmail.com>
2014-12-07 00:41:27 +0100

    Commit

--- src/com/tresors/repair/controller/RepairController.java
+++ src/com/tresors/repair/controller/RepairController.java
@@ -16,7 +16,7 @@ public class RepairController {
 
     /**
      * Creates a RepairController with a model as parameter
-     * @param model
+     * @param model a RepairModel used by the controller as database
      */
     public RepairController(RepairModel model){
         this.model = model;
@@ -24,35 +24,71 @@ public class RepairController {
         addListenersToModel();
     }
 
+    /**
+     * A Method that adds listeners to the model
+     */
     private void addListenersToModel() {
         model.addRepairCanonListener(view);
         model.addRepairPirateListener(view);
     }
 
+    /**
+     * Displays the required view
+     */
     public void displayViews(){
         view.display();
     }
 
+    /**
+     * Closes the window
+     */
     public void closeViews(){
         view.close();
     }
 
+    /**
+     * A Method that indicates to the model that data has been modified
+     * @param nbPirates the new amount of pirates on the ship
+     */
     public void notifyPiratesChanged(int nbPirates){
         model.setNbPirates(nbPirates);
     }
+    /**
+     * A Method that indicates to the model that data has been modified
+     * The number of Pirates on a Ship have been Increased to nbPirates
+     * @param nbPirates the new amount of pirates on the ship
+     */
     public void notifyPiratesIncreased(int nbPirates){
         model.setNbPirates(nbPirates);
     }
+    /**
+     * A Method that indicates to the model that data has been modified
+     * The number of Pirates on a Ship have been Decreased to nbPirates
+     * @param nbPirates the new amount of pirates on the ship
+     */
     public void notifyPiratesDecreased(int nbPirates){
         model.setNbPirates(nbPirates);
     }
-
+    /**
+     * A Method that indicates to the model that data has been modified
+     * @param nbCanons the new amount of canons on the ship
+     */
     public void notifyCanonsChanged(int nbCanons){
         model.setNbCanons(nbCanons);
     }
+    /**
+     * A Method that indicates to the model that data has been modified
+     * The number of Pirates on a Ship have been Increased to nbCanons
+     * @param nbCanons the new amount of canons on the ship
+     */
     public void notifyCanonsIncreased(int nbCanons){
         model.setNbCanons(nbCanons);
     }
+    /**
+     * A Method that indicates to the model that data has been modified
+     * The number of Pirates on a Ship have been Decreased to nbCanons
+     * @param nbCanons the new amount of canons on the ship
+     */
     public void notifyCanonsDecreased(int nbCanons){
         model.setNbCanons(nbCanons);
     }
