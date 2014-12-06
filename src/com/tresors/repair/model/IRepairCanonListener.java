0fb4b8cb7f311657f069f8b14f8004775467e79b
Paul <pribierre@gmail.com>
2014-12-07 00:47:23 +0100

    Commit Javadoc Ajout de Javadoc

--- src/com/tresors/repair/model/IRepairCanonListener.java
+++ src/com/tresors/repair/model/IRepairCanonListener.java
@@ -1,6 +1,11 @@
 package com.tresors.repair.model;
 import java.util.EventListener;
 
+/**
+ * An Interface that contains the methods that should be implemented when an Event asks for a modification of the model
+ * Created by Paul on 06/12/2014.
+ * @author Paul Ribierre
+ */
 public interface IRepairCanonListener extends EventListener {
     public void canonsChanged(RepairChangeNbCanonEvent event);
     public void canonsDecreased(RepairChangeNbCanonEvent event);
