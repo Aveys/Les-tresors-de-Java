76104fa7490e39a3f247de51a198714f66aad930
acrepina <acrepina@yahoo.fr>
2014-12-09 11:48:56 +0100

    Debut test HexToolbox

---
+++ test/com/tresors/controller/HexToolboxTest.java
@@ -0,0 +1,15 @@
+package com.tresors.controller;
+
+import org.junit.*;
+
+/**
+ * Created by aurelia on 09/12/2014.
+ */
+public class HexToolboxTest {
+    @Test
+    public void getVoisinsTest() {
+       // Assert.assertEquals("Type de case incorrect",'P',pl.getTypeCase(1,1));
+    }
+
+
+}
