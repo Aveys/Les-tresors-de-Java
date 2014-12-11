60f8b6a19c0ab3beae4b10a487001e70b7f7a69f
Paul <pribierre@gmail.com>
2014-12-11 15:23:57 +0100

    Javadoc Ajout de la javadoc

--- src/com/tresors/event/IPositionChangedListener.java
+++ src/com/tresors/event/IPositionChangedListener.java
@@ -3,7 +3,9 @@ package com.tresors.event;
 import java.util.EventListener;
 
 /**
+ * An Interface that contains the methods that should be implemented when an Event asks for a modification of the model
  * Created by Paul on 11/12/2014.
+ * @author Paul Ribierre
  */
 public interface IPositionChangedListener extends EventListener {
     public void positionChanged(ChargePositionChangedEvent event);
